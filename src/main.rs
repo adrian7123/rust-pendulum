b13e74028b9a2ddbca2b94697b7e26cea4e7c265
Adrian_Bueno <adrian.s.bueno1@gmail.com>
2023-08-04 18:08:09 -0300

    separe modules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+mod pendulum;
+use pendulum::Pendulum;
 use speedy2d::color::Color;
 use speedy2d::window::{WindowHandler, WindowHelper};
 use speedy2d::Graphics2D;
 use speedy2d::Window;
-use vector::Vector;
 
 fn main() {
     let window = Window::<()>::new_centered("Pendulum", (800, 480)).unwrap();
@@ -32,58 +33,6 @@ impl WindowHandler for MyWindowHandler {
         helper.request_redraw();
     }
 }
-
-struct Pendulum {
-    origin: Vector,
-    position: Vector,
-
-    angle: f32,
-    angular_velocity: f32,
-    angular_acceleration: f32,
-    r: f32,
-    // m: f32,
-    g: f32,
-}
-
-impl Pendulum {
-    fn new(x: f32, y: f32, r: f32) -> Pendulum {
-        Pendulum {
-            origin: Vector::new(x, y),
-            position: Vector::new(x, y),
-            angle: 1.0,
-            angular_velocity: 0.0,
-            angular_acceleration: 0.0,
-            r: r,
-            // m: 1.0,
-            g: 1.5,
-        }
-    }
-
-    fn update(&mut self) {
-        self.angular_acceleration = -1.0 * self.g * self.angle.sin() / self.r;
-
-        self.angular_velocity += self.angular_acceleration;
-
-        self.angle += self.angular_velocity;
-
-        self.position
-            .set(self.r * self.angle.sin(), self.r * self.angle.cos());
-
-        self.position.add(&self.origin);
-    }
-
-    fn draw(&self, graphics: &mut Graphics2D) {
-        graphics.draw_line(
-            (self.origin.x, self.origin.y),
-            (self.position.x, self.position.y),
-            3.0,
-            Color::RED,
-        );
-
-        graphics.draw_circle((self.position.x, self.position.y), 30.0, Color::RED)
-    }
-}
-
 mod vector {
     pub struct Vector {
         pub x: f32,
---
+++ src/pendulum.rs
@@ -0,0 +1,53 @@
+use speedy2d::{color::Color, Graphics2D};
+
+use crate::vector::Vector;
+pub struct Pendulum {
+    origin: Vector,
+    position: Vector,
+
+    angle: f32,
+    angular_velocity: f32,
+    angular_acceleration: f32,
+    r: f32,
+    // m: f32,
+    g: f32,
+}
+
+impl Pendulum {
+    pub fn new(x: f32, y: f32, r: f32) -> Pendulum {
+        Pendulum {
+            origin: Vector::new(x, y),
+            position: Vector::new(x, y),
+            angle: 1.0,
+            angular_velocity: 0.0,
+            angular_acceleration: 0.0,
+            r: r,
+            // m: 1.0,
+            g: 1.5,
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.angular_acceleration = -1.0 * self.g * self.angle.sin() / self.r;
+
+        self.angular_velocity += self.angular_acceleration;
+
+        self.angle += self.angular_velocity;
+
+        self.position
+            .set(self.r * self.angle.sin(), self.r * self.angle.cos());
+
+        self.position.add(&self.origin);
+    }
+
+    pub fn draw(&self, graphics: &mut Graphics2D) {
+        graphics.draw_line(
+            (self.origin.x, self.origin.y),
+            (self.position.x, self.position.y),
+            3.0,
+            Color::RED,
+        );
+
+        graphics.draw_circle((self.position.x, self.position.y), 30.0, Color::RED)
+    }
+}
